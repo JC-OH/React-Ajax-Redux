fbd227610a81728be227b690241f38cbd68705bc
Jicheol Oh <jicheol@gmail.com>
2019-06-28 12:32:02 -0700

    CommentList 컴포넌트 만들기

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Header from './Header/Header';
 import PostWrapper from './PostWrapper/PostWrapper';
 import Navigator from './Navigator/Navigator';
 import Post from './Post/Post';
+import CommentList from './CommentList/CommentList';
 
 export {
     Header,
     PostWrapper,
     Navigator,
-    Post
+    Post,
+    CommentList
 };
