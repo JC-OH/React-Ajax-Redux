16a3a08d1d2550e2c3f691d421aeafc85df9ac98
Jicheol Oh <jicheol@gmail.com>
2019-06-28 12:17:17 -0700

    똑똑한 컴포넌트, PostContainer 만들기

---
+++ src/containers/PostContainer/PostContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { PostWrapper } from '../../components';
+
+class PostContainer extends Component {
+    render() {
+        return (
+            <PostWrapper>
+                Hello, Post
+            </PostWrapper>
+        )
+    }
+}
+export default PostContainer;
